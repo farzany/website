f59fd20b71a2c4d1a317aa099717525ac019366d
farzany <59945358+farzany@users.noreply.github.com>
2020-07-06 20:10:09 -0400

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,20 @@
+// function testExample(a) {
+//     var greeting = "Hi! My name is " + a;
+//     return greeting;
+// }
+
+// var name = "Yaz";
+// console.log(testExample(name));
+
+// var testExample = function() {
+//     var greeting = "Hi! My name is Yaz";
+//     return greeting
+// }
+
+// console.log(testExample());
+
+(function() {
+    var greeting = "Hi! My name is Yaz";
+    console.log(greeting);
+}())
+
