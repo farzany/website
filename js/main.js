9dd07ba83134839e82fc45b552d54dd7b5fed2d5
farzany <59945358+farzany@users.noreply.github.com>
2021-06-05 19:57:46 -0400

    Redesigned Website

--- js/main.js
+++ js/main.js
@@ -12,29 +12,3 @@ $(document).ready(function(){
 AOS.init({
     duration: 1000,
 });
-
-// Resume Currently Unavailable
-let resume = document.getElementById("resume");
-if (resume !== null) {
-    resume.addEventListener('click',function(){
-        resume.innerHTML = "Currently Unavailable"
-        setTimeout(function () {
-            resume.innerHTML = "Resume"
-        },1500);
-    });
-}
-
-// Modals
-function ModalOpen(obj) {
-    let modalId = obj.id;
-    console.log(modalId);
-    let modalBg = document.getElementsByClassName("modal-bg " + modalId)[0];
-    console.log(modalBg);
-    let modalClose = modalBg.getElementsByClassName("modal-close")[0];
-
-    modalBg.classList.add('modal-active');
-
-    modalClose.addEventListener('click',function() {
-        modalBg.classList.remove('modal-active');
-    });
-}
